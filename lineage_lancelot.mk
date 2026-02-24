887a4168789f9996b29ee72b34e27210db325611
Lenka-k <84929866+Lenka-k@users.noreply.github.com>
2026-02-24 05:54:55 +0530

    updating the file to make it compatible for shiva

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -9,20 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
-$(call inherit-product, device/xiaomi/lancelot/device.mk)
+$(call inherit-product, device/xiaomi/shiva/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
-PRODUCT_DEVICE := lancelot
+PRODUCT_NAME := crDroid_shiva
+PRODUCT_DEVICE := shiva
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi 9
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO M2
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lancelot-user 11 RP1A.200720.011 V12.5.6.0.RJCMIXM release-keys"
+    PRIVATE_BUILD_DESC="shiva-user 11 RP1A.200720.011 V12.5.6.0.RJRINXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/lancelot_global/lancelot:11/RP1A.200720.011/V12.5.6.0.RJCMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/shiva/shiva:11/RP1A.200720.011/V12.5.6.0.RJRINXM:user/release-keys
